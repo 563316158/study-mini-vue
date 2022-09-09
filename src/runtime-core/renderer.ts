a504480088369374fd0e14adae7e3dbe38fd68ae
Jerry.Qin 秦利杰 <jerry.qin@youniverse.cc>
2022-09-09 20:54:00 +0800

    学习完成shapeFlag

--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,4 +1,5 @@
 import { isObject } from "../shared/index";
+import { ShapeFlags } from "../shared/ShapeFlags";
 import { createComponentInstance, setupComponent } from "./component";
 
 export function render(vnode, container) {
@@ -10,9 +11,26 @@ function patch(vnode: any, container: any) {
   // TODO 判断vnode 是不是一个 elemnt
   // 是element 那么就应该处理 element
   // 思考题：如何区分是 element 还是 component 类型呢？
-  if (typeof vnode.type === "string") {
+
+  // ShapeFlags
+  // vnode -> flag
+  // element
+  const { shapeFlag } = vnode;
+  console.log(
+    vnode.type,
+    shapeFlag,
+    ShapeFlags.ELEMENT,
+    shapeFlag & ShapeFlags.ELEMENT
+  );
+  console.log(
+    vnode.type,
+    shapeFlag,
+    ShapeFlags.STATEFUL_COMPONENT, 
+    shapeFlag & ShapeFlags.STATEFUL_COMPONENT
+  );
+  if (shapeFlag & ShapeFlags.ELEMENT) {
     processElement(vnode, container);
-  } else if (isObject(vnode.type)) {
+  } else if (shapeFlag & ShapeFlags.STATEFUL_COMPONENT) {
     // 去处理组件
     processComponent(vnode, container);
   }
@@ -29,35 +47,30 @@ function mountComponent(vnode: any, container) {
   const instance = createComponentInstance(vnode);
 
   setupComponent(instance);
-  setupRenderEffect(instance, vnode,container);
+  setupRenderEffect(instance, vnode, container);
 }
 
-function setupRenderEffect(instance, vnode,container) {
+function setupRenderEffect(instance, vnode, container) {
   const { proxy } = instance;
   const subTree = instance.render.call(proxy);
 
-  console.log('subTree1:',subTree);
-
-  
   // vnode -> patch
   // vnode -> element -> mountElement
 
   patch(subTree, container);
 
-  console.log('subTree2:',subTree.el);
   vnode.el = subTree.el;
 }
 
 function mountElement(vnode: any, container: any) {
   const el = (vnode.el = document.createElement(vnode.type));
-  console.log("vnode",vnode);
 
   // string array
-  const { children } = vnode;
+  const { children,shapeFlag } = vnode;
 
-  if (typeof children === "string") {
+  if (shapeFlag&ShapeFlags.TEXT_CHILDREN) {
     el.textContent = children;
-  } else if (Array.isArray(children)) {
+  } else if (shapeFlag&ShapeFlags.ARRAY_CHILDREN) {
     // vnode
     mountChildren(vnode, el);
   }
@@ -76,5 +89,3 @@ function mountChildren(vnode, container) {
     patch(v, container);
   });
 }
-
-
